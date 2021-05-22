30ea49ad143fb6ffe69d30022f70dd01dd4bdb00
calm-prog <61384685+calm-prog@users.noreply.github.com>
2021-05-22 05:49:37 +0200

    Styled LoginIntro component
    - Updated sass in package.json
    - Updated Google font
    - Created an scss file for global variables
    - Added styles to LoginIntro
    - Adjusted styles of LoginCard

--- src/components/login/LoginIntro.tsx
+++ src/components/login/LoginIntro.tsx
@@ -1,8 +1,17 @@
 import './LoginIntro.scss'
+import login from '../../login.svg'
 
 const LoginIntro = () => {
     return (
-        <div className="login-intro">
+        <div className="login-intro-container">
+            <div className="login-intro">
+                <div className="login-intro-heading">
+                    Welcome back!<br/>
+                    Nice to see you again, we hope<br/>
+                    you are doing great
+                </div>
+                <img src={login} alt="login"/>
+            </div>
         </div>
     )
 }   
